e3771c164834fdcdf0d122e4e99872aa9c11713b
peter max <petermax2004@gmail.com>
2019-07-25 08:15:05 +0300

    Add

--- news/forms.py
+++ news/forms.py
@@ -3,3 +3,11 @@ from django import forms
 class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label='First Name',max_length=30)
     email=forms.EmailField(label='Email')
+    
+class NewsArticleForm(forms.ModelForm):
+    class meta:
+        model=Article
+        exclude=['editor','pub_date']
+        widgets={
+            'tags':forms.CheckboxSelectMultiple(),
+        }
--- news/views.py
+++ news/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse,Http404
+from django.http import HttpResponse,Http404,HttpResponseRedirect
 import datetime as dt
-from .models import Article
+from .models import Article,NewsLetterRecipients
+from news.forms import NewsLetterForm,NewArticleForm
+from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 # def welcome(request):
@@ -41,7 +45,7 @@ def past_days_news(request,past_date):
         
     if date == dt.date.today():
         return redirect(news_today)
-    
+
     day = convert_dates(date)
     html = f'''
         <html>
@@ -49,7 +53,7 @@ def past_days_news(request,past_date):
                 <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
-          
+
          '''
     news=Article.days_news(date)
     return render(request,'all-news/past-news.html',{"date":date,"news":news})
@@ -57,22 +61,47 @@ def past_days_news(request,past_date):
 def news_today(request):
     date=dt.date.today()
     news=Article.todays_news(date)
-    return render(request,'all-news/today-news.html',{"date":date,"news":news})
-
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient.save()
+            HttpResponseRedirect('news_today')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
 def search_results(request):
     if 'article' in request.GET and request.GET["article"]:
         search_term=request.GET.get("article")
         searched_articles=Article.search_by_title(search_term)
         message=f"{search_term}"
         return render(request,'all-news/search.html',{"message":message,"articles":searched_articles})
-    
+
     else:
         message="You have not searched for any term"
         return render(request,'all-news/search.html',{"message":message})
 
+@login_required(login_url='/accounts/login/')
 def article(request,article_id):
     try:
         article=Article.objects.get(id=article_id)
     except DoesNotExist:
         raise Http404()
     return render(request,"all-news/article.html",{"article":article})
+
+@login_required(login_url='/accounts/login/')
+def new_article(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.editor = current_user
+            article.save()
+        return redirect('NewsToday')
+
+    else:
+        form = NewArticleForm()
+    return render(request, 'new_article.html', {"form": form})
